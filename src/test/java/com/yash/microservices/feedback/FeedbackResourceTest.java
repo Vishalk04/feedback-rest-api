63b65eb742ceeae6be029c9feeb4525c39d5bc3d
Vishalk04 <42270298+Vishalk04@users.noreply.github.com>
2019-01-28 10:57:34 +0530

    Update FeedbackResourceTest.java

--- src/test/java/com/yash/microservices/feedback/FeedbackResourceTest.java
+++ src/test/java/com/yash/microservices/feedback/FeedbackResourceTest.java
@@ -74,11 +74,6 @@ public class FeedbackResourceTest {
 		assertTrue(feedbackResourceThree.equals(feedbackResourceOne));
 	}
 	
-	@Test
-	public void testConsistant() throws ParseException {
-	
-	}
-	
 	
 	@Test
 	public void testNonNullity() throws ParseException {
@@ -87,18 +82,21 @@ public class FeedbackResourceTest {
 		FeedbackResource feedbackResourceOne = new FeedbackResource("jd123", "John Deere", 5, "greate", date);
 		
 		assertFalse(feedbackResourceOne.equals(null));
+		
+		feedbackResourceOne.setUsername(null);
+		assertFalse(feedbackResourceOne.equals(feedbackResourceTwo));
 	}
 	
 	
-@Test
-public void testDifference() throws ParseException{
-	SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
-	Date date = format.parse("2019-01-19");
-	FeedbackResource feedbackResourceOne = new FeedbackResource("jd123", "John Deere", 5, "great", date);
-	FeedbackResource feedbackResourceTwo = new FeedbackResource("ysh123", "Yash", 5, "great", date);
-	
-	assertFalse(feedbackResourceOne.equals(feedbackResourceTwo));
-}
+	@Test
+	public void testDifference() throws ParseException{
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+		Date date = format.parse("2019-01-19");
+		FeedbackResource feedbackResourceOne = new FeedbackResource("jd123", "John Deere", 5, "great", date);
+		FeedbackResource feedbackResourceTwo = new FeedbackResource("ysh123", "Yash", 5, "great", date);
+
+		assertFalse(feedbackResourceOne.equals(feedbackResourceTwo));
+	}
 	@Test
 	public void testEquality() throws ParseException {
 
@@ -111,43 +109,11 @@ public void testDifference() throws ParseException{
 		assertTrue(feedbackResourceOriganal.equals(feedbackResourceNew));
 		assertTrue(feedbackResourceOriganal.hashCode() == feedbackResourceNew.hashCode());
 		
-
-		feedbackResourceOriganal.setComment("good");
-		assertFalse(feedbackResourceOriganal.equals(feedbackResourceNew));
-		assertFalse(feedbackResourceOriganal.hashCode() == feedbackResourceNew.hashCode());
-		feedbackResourceOriganal.setComment("great");
-	
-		feedbackResourceOriganal.setRating(1);
-		assertFalse(feedbackResourceOriganal.equals(feedbackResourceNew));
-		assertFalse(feedbackResourceOriganal.hashCode() == feedbackResourceNew.hashCode());
-		feedbackResourceOriganal.setRating(5);
-	
-		feedbackResourceOriganal.setSource("yash");
-		assertFalse(feedbackResourceOriganal.equals(feedbackResourceNew));
-		assertFalse(feedbackResourceOriganal.hashCode() == feedbackResourceNew.hashCode());
-		feedbackResourceOriganal.setSource("John Deere");
-	
-		Date createdTime = format.parse("2019-01-01");
-		feedbackResourceOriganal.setCreatedTime(createdTime);
-		assertFalse(feedbackResourceOriganal.equals(feedbackResourceNew));
-		assertFalse(feedbackResourceOriganal.hashCode() == feedbackResourceNew.hashCode());
-		feedbackResourceOriganal.setCreatedTime(date);
-			
-	}
-	
-	@Test
-	public void testEqualityForUserName() throws ParseException {
-		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
-
-		Date date = format.parse("2019-01-19");
-		FeedbackResource feedbackResourceOriganal = new FeedbackResource("jd123", "John Deere", 5, "great", date);
-		FeedbackResource feedbackResourceNew = new FeedbackResource("jd123", "John Deere", 5, "great", date);
-
+		
 		feedbackResourceOriganal.setUsername("user123");
 		assertFalse(feedbackResourceOriganal.equals(feedbackResourceNew));
 		assertFalse(feedbackResourceOriganal.hashCode() == feedbackResourceNew.hashCode());
-		feedbackResourceOriganal.setUsername("jd123");
-	
+			
 	}
 
 }
