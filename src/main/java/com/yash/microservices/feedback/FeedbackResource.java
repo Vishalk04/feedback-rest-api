4e5d12fed306219b0a87f0b7e84869d9f66e5f97
Vishalk04 <42270298+Vishalk04@users.noreply.github.com>
2019-01-28 11:59:47 +0530

    Update FeedbackResource.java

--- src/main/java/com/yash/microservices/feedback/FeedbackResource.java
+++ src/main/java/com/yash/microservices/feedback/FeedbackResource.java
@@ -19,6 +19,17 @@ public class FeedbackResource extends ResourceSupport {
     private Date createdTime;
     
     @Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + ((comment == null) ? 0 : comment.hashCode());
+		result = prime * result + ((createdTime == null) ? 0 : createdTime.hashCode());
+		result = prime * result + ((rating == null) ? 0 : rating.hashCode());
+		result = prime * result + ((source == null) ? 0 : source.hashCode());
+		result = prime * result + ((username == null) ? 0 : username.hashCode());
+		return result;
+	}
+	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
 			return true;
@@ -27,21 +38,31 @@ public class FeedbackResource extends ResourceSupport {
 		if (getClass() != obj.getClass())
 			return false;
 		FeedbackResource other = (FeedbackResource) obj;
-			
+		if (comment == null) {
+			if (other.comment != null)
+				return false;
+		} else if (!comment.equals(other.comment))
+			return false;
+		if (createdTime == null) {
+			if (other.createdTime != null)
+				return false;
+		} else if (!createdTime.equals(other.createdTime))
+			return false;
+		if (rating == null) {
+			if (other.rating != null)
+				return false;
+		} else if (!rating.equals(other.rating))
+			return false;
+		if (source == null) {
+			if (other.source != null)
+				return false;
+		} else if (!source.equals(other.source))
+			return false;
 		if (username == null) {
 			if (other.username != null)
 				return false;
 		} else if (!username.equals(other.username))
 			return false;
-		
 		return true;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = super.hashCode();
-		result = prime * result + ((username == null) ? 0 : username.hashCode());
-		return result;
 	}
 }
