9786a264fdf44bf356aee500f2de1f704a76acc1
Vishalk04 <42270298+Vishalk04@users.noreply.github.com>
2019-01-28 11:09:56 +0530

    Update schema-postgres.sql

--- src/main/resources/schema-postgres.sql
+++ src/main/resources/schema-postgres.sql
@@ -1,3 +1,3 @@
-CREATE SEQUENCE feedback_id_seq;
+CREATE SEQUENCE feedback_id_seq START 1;
 CREATE SCHEMA feedback_schema AUTHORIZATION root;
 CREATE TABLE feedback_schema.feedback(id integer DEFAULT nextval('feedback_id_seq'), source varchar(255), rating integer, comment varchar(255), username varchar(255), created_time date,  PRIMARY KEY (id));
